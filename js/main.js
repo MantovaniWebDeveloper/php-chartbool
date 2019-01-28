d21f408c732c71358d2c52e550d8cc1b8b2b8c51
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-28 11:09:43 +0100

    ora i dati sono passati al grafico

--- js/main.js
+++ js/main.js
@@ -2,26 +2,6 @@ $(document).ready(function() {
   //indirizzo locale dei dati
   var url = 'http://localhost/php/php-chartbool/data.php';
   recuperoDati(url);
-  //parte esempio da modificare
-  var ctx = $(".wrapGrafico");
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [{
-        label: "My First dataset",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-      }]
-    },
-
-    // Configuration options go here
-    options: {}
-  });
 
 
 
@@ -33,6 +13,28 @@ $(document).ready(function() {
       success: function(data) {
         var dati = JSON.parse(data);
         console.log(dati);
+
+        //parte esempio da modificare
+        var ctx = $(".wrapGrafico");
+        var chart = new Chart(ctx, {
+          // The type of chart we want to create
+          type: 'line',
+
+          // The data for our dataset
+          data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+              label: "My First dataset",
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data:  dati,
+            }]
+          },
+
+          // Configuration options go here
+          options: {}
+        });
+
       },
       error: function(error) {
         alert("c'è un errore di comunicazione");
