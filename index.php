91b4f138c77f0ee0297d347eb405022829ac224f
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-28 10:07:38 +0100

    creata area stampa grafico

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 <html lang="it" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>PHP-CHARTBOOL BY DARIO MANTOVANI</title>
   </head>
   <body>
+    <div class="container">
+      <div class="wrapGrafico">
 
-    <?php echo "ciao"; ?>
-  <script src"js/main.js"></script>
+      </div>
+    </div>
+  <script src="js/main.js">
+  </script>
   </body>
 </html>
