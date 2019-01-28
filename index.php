4623e2d4a6c5fe8c486eec551af22a7b0b692e93
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-28 09:42:38 +0100

    init progetto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP-CHARTBOOL BY DARIO MANTOVANI</title>
+  </head>
+  <body>
+
+    <?php echo "ciao"; ?>
+  <script src"js/main.js"></script>
+  </body>
+</html>
